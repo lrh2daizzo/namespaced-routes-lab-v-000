3b37e14477a33f8efc580d6d389a5335b0757ce7
lrh2daizzo <herrera.lrh@gmail.com>
2021-05-07 16:05:18 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
   resources :songs
 
-  namespace :admin do
-    resources :preferences, only: [:index]
+  namespace :admin do 
+    resources :preferences, only: [:index] 
   end
 end
